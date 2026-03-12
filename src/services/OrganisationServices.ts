f9a4009047383eeb42ab6849d1759840824ca520
Moipone Ruth Motingoe <ruthmotingoe@gmail.com>
2026-03-12 20:46:31 +0200

    Avatar initials (#8)
    * feat: update SignUpPage to replace Trophy icons with logo images and add new image asset
    
    * feat: update SignUpPage to use new logo image for branding
    
    * feat: refactor SignUpPage for improved form handling and UI consistency
    
    * feat: add organisationServices module with fetchOrganisation function for API integration
    
    * feat: add user avatar fallback in ProfilePage for improved user experience

---
+++ src/services/OrganisationServices.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { ENDPOINTS } from "@/utils/endpoints";
+import useUserStore from "@/context/userStore";
+
+const organisationServices = {
+    fetchOrganisation: async () => {
+        try {
+            const { getOrganisation } = ENDPOINTS;
+            const { token } = useUserStore.getState();
+
+            const organisation = await axios.get(getOrganisation, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            return {
+                success: organisation.data.success,
+                data: organisation.data,
+                error: organisation.data.message || null,
+            };
+        } catch (error: any) {
+            return {
+                success: false,
+                error:
+                    error.response?.data?.message ||
+                    error.message ||
+                    "Organisation not found",
+            };
+        }
+    },
+};
+
+export default organisationServices;
